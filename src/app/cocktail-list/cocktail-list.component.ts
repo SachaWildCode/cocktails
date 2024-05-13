f484147b0741be8b69629633ebc3775598a5fc0d
SachaWildCode <sacha.loumachi@gmail.com>
2024-05-13 16:44:57 +0200

    finish

--- src/app/cocktail-list/cocktail-list.component.ts
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CocktailService } from '../cocktail.service';
 import { CommonModule } from '@angular/common';
+import { Cocktails } from '../interfaces/cocktails';
 
 @Component({
-  selector: 'app-cocktail-list-component',
+  selector: 'app-cocktail-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './cocktail-list.component.html',
   styleUrl: './cocktail-list.component.css',
 })
 export class CocktailListComponent {
-  constructor(private CocktailService: CocktailService) {}
-  cocktails = this.CocktailService.getCocktails();
+  cocktails: Cocktails[] = [];
+
+  private cocktailService = inject(CocktailService);
+
+  ngOnInit(): void {
+    this.cocktailService.getCocktails().subscribe((cocktailsFromJsonFile) => {
+      this.cocktails = cocktailsFromJsonFile;
+    });
+  }
 }
--- src/app/cocktail.service.ts
+++ src/app/cocktail.service.ts
@@ -1,28 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Cocktails } from './interfaces/cocktails';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CocktailService {
+  private http = inject(HttpClient);
+
   constructor() {}
 
-  private margherita: Cocktails = {
-    name: 'Margherita',
-    img: 'https://www.averiecooks.com/wp-content/uploads/2018/01/margherita-pizza-5.jpg',
-    price: 10,
-  };
-  private peperoni: Cocktails = {
-    name: 'Peperoni',
-    img: 'https://www.averiecooks.com/wp-content/uploads/2018/01/margherita-pizza-5.jpg',
-    price: 20,
-  };
-  private hawaiian: Cocktails = {
-    name: 'Hawaiian',
-    img: 'https://www.averiecooks.com/wp-content/uploads/2018/01/margherita-pizza-5.jpg',
-    price: 10,
-  };
-  getCocktails(): Array<Cocktails> {
-    return [this.peperoni, this.margherita, this.hawaiian];
+  getCocktails(): Observable<Cocktails[]> {
+    return this.http.get<Cocktails[]>('assets/cocktails.json');
   }
 }
